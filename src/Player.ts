9fe7de9327330f3290b315194bdc58144081c8ec
Jenny <cookie.monzta@web.de>
2017-10-14 14:06:51 +0200

    mitgehen nur um 10 erhöhen

--- src/Player.ts
+++ src/Player.ts
@@ -7,8 +7,8 @@ export class Player {
             //if (highestBet === 1000) {
             //    betCallback(1000);
             //}
-            if (highestBet + 100 <= 1000) {
-                betCallback(highestBet + 100);
+            if (highestBet + 10 <= 1000) {
+                betCallback(highestBet + 10);
             }
             betCallback(10);
         } catch (e) {
