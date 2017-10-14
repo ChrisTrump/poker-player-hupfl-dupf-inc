0eb68f93edf020d4f4b9476bbff7af2903170bc3
Christian Trump <trumpch61131@gmail.com>
2017-10-14 13:06:11 +0200

    started working on raise 100 if pair else 10.

--- src/Player.ts
+++ src/Player.ts
@@ -1,5 +1,16 @@
 export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
+
+    const players: any[] = gameState.players;
+    const ourPlayer = players.filter(player => player.name == "Hupfl Dupf Inc");
+    const cards: any[] = ourPlayer[0].hole_cards;
+    const isPair = cards[0].rank == cards[1].rank;
+
+      if (isPair) {
+          betCallback(100);
+      }
+
+
     betCallback(100);
   }
 
